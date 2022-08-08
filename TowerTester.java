9a8a2fdc63f4c36518ff8f71e2961f19bffd82c8
CMWL1970 <64796913+cmwl1970@users.noreply.github.com>
2022-08-08 09:09:50 -0700

    Add files via upload

---
+++ TowerTester.java
@@ -0,0 +1,29 @@
+// Programmer: Dylan Canfield
+// Class: CS &145
+// Date 8/**/2022
+// Assignment: Towers of Hanoi
+// display use of recursive algorithm
+// using the "Towers of Hanoi" problem
+// puzzle involves moving discs between 3 towers
+// discs have incrementing diameters with
+// the smallest disc on top.  Larger discs
+// cannot be stacked on smaller discs
+// User input for amount of discs up to 5
+// Display solution for amount of discs
+
+// **** CODE OF NOTE ****
+// minimal main method in TowerTester
+// boolean expressions for logic flow
+// Try+Catch for Integer (57-66)
+// Only prints intro and asciiTowers once
+// enhanced for loop with regex (98)
+// **** CODE OF NOTE ****
+
+public class TowerTester {
+
+public static void main(String[] args) {
+    TowerManager.intro();
+    TowerManager.menu();
+} // end of main method
+} // end class TowerTester
+
